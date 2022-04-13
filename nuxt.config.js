1f132a8c356eda4742cf670bd960eb6cb7406398
em lừa thầy ah <103566407+irmalpephriam5512@users.noreply.github.com>
2022-04-13 15:45:45 +0700

    Create nuxt.config.js

---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+import webpack from 'webpack'
+
+export default {
+  head: {
+    titleTemplate: '%s - qtum.info',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'}
+    ]
+  },
+  css: [
+    'bulma/css/bulma.css',
+    '@fortawesome/fontawesome-free/css/all.css',
+    '@/styles/common.less',
+    '@/styles/card.less',
+    '@/styles/info-table.less',
+    '@/icons/style.css'
+  ],
+  render: {
+    bundleRenderer: {
+      shouldPreload: (file, type) => ['script', 'style', 'font'].includes(type)
+    }
+  },
+  build: {
+    extend(config, {isServer}) {
+      config.module.rules.push({
+        test: /\.ya?ml$/,
+        type: 'json',
+        use: 'yaml-loader'
+      })
+      config.plugins.push(new webpack.DefinePlugin({
+        'process.env.qtuminfoAPIBase': JSON.stringify(process.env.QTUMINFO_API_BASE
+          || process.env[isServer ? 'QTUMINFO_API_BASE_SERVER' : 'QTUMINFO_API_BASE_CLIENT']
+          || 'http://localhost:7001/'),
+        'process.env.qtuminfoWSBase': JSON.stringify(process.env.QTUMINFO_WS_BASE
+          || process.env.QTUMINFO_API_BASE_WS
+          || '//localhost:7001/'),
+        'process.env.network': JSON.stringify(process.env.QTUM_NETWORK || 'mainnet')
+      }))
+    },
+    extractCSS: true,
+    postcss: {
+      features: {
+        customProperties: false
+      }
+    }
+  },
+  serverMiddleware: ['~/middleware/ip.js'],
+  plugins: [
+    '~/plugins/components.js',
+    '~/plugins/i18n.js',
+    '~/plugins/qtum-utils.js',
+    {src: '~/plugins/websocket.js', ssr: false}
+  ]
+}
